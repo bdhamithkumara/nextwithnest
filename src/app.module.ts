79ecbad3b370cf6aa30dfe0422392a7aefde3b33
bdhamithkumara <damithkumararoxx55@gmail.com>
2025-04-07 23:04:31 +0530

    feat : update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { env } from 'process';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule, ConfigModule.forRoot({ envFilePath: ['.env'] })],
   controllers: [AppController],
   providers: [AppService],
 })
