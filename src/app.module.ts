b3ade2283297550cfbff68b039265d7e65cd169a
bdhamithkumara <damithkumararoxx55@gmail.com>
2025-04-07 23:12:41 +0530

    feat : fixx error npm install @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
-import { env } from 'process';
+
 
 @Module({
   imports: [ConfigModule, ConfigModule.forRoot({ envFilePath: ['.env'] })],
